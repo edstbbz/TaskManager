9ce06171c1b9a5908e0b93f237cd1a10e033840f
edstbbz <ed_stubbz@mail.ru>
2021-01-19 21:57:16 +0400

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
+  has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
+  validates :first_name, length: { minimum: 2 }, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true,
+                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },
+                    presence: true
 end
