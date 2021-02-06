7df95014d4eefc187d9c814691d480782caf3da2
edstbbz <ed_stubbz@mail.ru>
2021-02-07 02:03:48 +0400

    rubocop settings and style fix

--- app/controllers/web/boards_controller.rb
+++ app/controllers/web/boards_controller.rb
@@ -1,6 +1,5 @@
 class Web::BoardsController < Web::ApplicationController
-  
   def show
-    render react_component: 'TaskBoard', props: {}
+    render(react_component: 'TaskBoard', props: {})
   end
 end
